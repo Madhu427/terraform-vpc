0e127cf2fae20a9b23402997ff545aeae53953a7
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-12 18:57:18 +0530

    nat & route added

--- gateways.tf
+++ gateways.tf
@@ -6,3 +6,22 @@ resource "aws_internet_gateway" "gw" {
     ENV  = var.ENV
   }
 }
+
+resource "aws_eip" "nat-gw" {
+  vpc      = true
+  tags = {
+    Name = "${var.ENV}-nat-gw-ip"
+    ENV  = var.ENV
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.nat-gw.id
+  subnet_id     = aws_subnet.public-subnet.*.id[0]
+
+  tags = {
+    Name = "${ENV}-ngw"
+    ENV  = var.ENV
+  }
+
+}
