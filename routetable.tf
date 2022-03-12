deac18b652446248f2a8610c1e5cac4edfc9ba62
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-12 17:37:03 +0530

    route tables --> added

---
+++ routetable.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "public-table" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = {
+    Name = "public-rt"
+    ENV  = var.ENV
+  }
+}
+
+resource "aws_route_table" "private-table" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = {
+    Name = "priavte-rt"
+    ENV  = var.ENV
+  }
+}
