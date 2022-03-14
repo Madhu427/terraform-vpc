b0a7b436951735905fd3e7ad06a2c8457edcc199
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-14 12:23:58 +0530

    default -rt added

--- routetable.tf
+++ routetable.tf
@@ -57,3 +57,11 @@ resource "aws_route" "private-peer-route" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
 
 }
+
+resource "aws_route" "default-rtable-route" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+
+}
+
