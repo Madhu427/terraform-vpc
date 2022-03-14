3a1c8659ade389e24bcc4c13400a7c4f9293b775
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-14 11:53:46 +0530

    public & private peer added

--- routetable.tf
+++ routetable.tf
@@ -37,9 +37,23 @@ resource "aws_route" "public-igw-route" {
 
 }
 
-resource "aws_route" "privat-ngw-route" {
+resource "aws_route" "private-ngw-route" {
   route_table_id            = aws_route_table.private-table.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.ngw.id
+
+}
+
+resource "aws_route" "public-peer-route" {
+  route_table_id            = aws_route_table.public-table.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+
+}
+
+resource "aws_route" "private-peer-route" {
+  route_table_id            = aws_route_table.private-table.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
 
 }
