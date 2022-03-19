338866c96f447e87d3a1e53147b22f7dd3d653ac
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-20 00:55:18 +0530

    PRIVATE_HOSTED_ZONE_ID

--- outputs.tf
+++ outputs.tf
@@ -31,6 +31,6 @@ output "DEFAULT_VPC_CIDR" {
   value = data.aws_vpc.default.cidr_block
 }
 
-output "PRIVATE_HOST_ZONE_ID" {
+output "PRIVATE_HOSTED_ZONE_ID" {
   value = var.PRIVATE_HOSTED_ZONE_ID
 }
